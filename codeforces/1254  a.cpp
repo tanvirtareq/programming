c8dd92abedc66b2a7780f59cfe8648b4512fa449
Tanvir Tareq <cse.tanvir.17@gmail.com>
2019-12-26 00:00:23 +0600

    a

--- codeforces/1254  a.cpp
+++ codeforces/1254  a.cpp
@@ -1,72 +1,91 @@
 #include<bits/stdc++.h>
-#define ll long long
-#define lu long long unsigned
-#define dbg(a) cout<<#a<<"= "<<a<<endl
-
 using namespace std;
-
+int r, c, k, q, ar[200][200], br[200][200];
+char ch, cr[200][200];
 int main()
 {
-	#ifndef ONLINE_JUDGE 
-	freopen("input.txt", "r", stdin);
-	freopen("output.txt", "w", stdout);
-	#endif
+    cin>>q;
+    while(q--)
+    {
+        cin>>r>>c>>k;
+        int ct=0;
+        for(int i=1; i<=r; i++)
+            for(int j=1; j<=c; j++)
+            {
+                cin>>ch;
+                ct+=ar[i][j]=(ch=='R');
+            }
 
-	int q;
-	cin>>q;
-	while(q--)
-	{
-		int r, c, k;
-		cin>>r>>c>>k;
-		char ch, ar[200][200];
-		char f='$';
-		for(int i=1;i<=r;i++)
-			for(int j=1;j<=c;j++)
-			{
-				char c;
-				cin>>c;
-				ar[i][j]=c;
-				if(c=='R' and k>0)
-				{
-					if(k>52) ar[i][j]='0'+(k-53);
-					else if(k>26) ar[i][j]='a'+(k-27);
-					else if(k>0) ar[i][j]='A'+(k-1);
-					k--;
-					if(f=='$') f=ar[i][j]; 
-				}
-			}
-		int i=1, j=1;
-		int fl=0;
-		for(;i<=r;i++){
-			for(;j<=c;j++){
-				if(ar[i][j]==f) { fl=1; break;}
-				ar[i][j]=f;
-			}
-			if(fl) break;
-		}
-		// dbg(i);
-		// dbg(j);
+        int i=1,id=1, j=1,ft=0, f=0, flg=0,tk=ct/k, xtra=ct%k;
+        br[1][1]=id;
+        if(ar[1][1]==1)
+        {
+            flg=1;
+        }
 
-		for(int i=1;i<=r;i++)
-		{
-			for(int j=1;j<=c;j++)
-			{
-				if(ar[i][j]=='.' or ar[i][j]=='R')
-				{
-					if(j>1) ar[i][j]=ar[i][j-1];
-					else if(i>1) ar[i][j]=ar[i-1][j];
-					// dbg(i);
-					// dbg(j);
-					// dbg(ar[i][j]);
-				}
-			}
-			// dbg(i);
-		}
-		for(int i=1;i<=r;i++){
-			for(int j=1;j<=c;j++) cout<<ar[i][j];
-			cout<<endl;
-		}
-	}
-	
-	return 0;
+        while(1)
+        {
+//            cout<<i<<" "<<j<<endl;
+            if(!f)
+            {
+                if(j==c)
+                {
+                    if(i==r)
+                        break;
+                    i++;
+                    f=1;
+                }
+                else
+                    j++;
+            }
+            else
+            {
+
+                if(j==1)
+                {
+                    if(i==r)
+                        break;
+                    i++;
+                    f=0;
+                }
+                else
+                    j--;
+
+            }
+
+
+            if(ar[i][j]==1 and flg==1)
+            {
+                if(xtra)
+                    ft=(ft+1)%(tk+1);
+                else
+                    ft=(ft+1)%tk;
+                if(ft==0)
+                    xtra=max(0, xtra-1), id++;
+            }
+            if(ar[i][j]==1 and flg==0) flg=1;
+
+             br[i][j]=id;
+        }
+
+        for(int i=1; i<=r; i++)
+        {
+            for(int j=1; j<=c; j++)
+            {
+                int k=br[i][j];
+                if(k>52)
+                    cr[i][j]='0'+(k-53);
+                else if(k>26)
+                    cr[i][j]='a'+(k-27);
+                else if(k>0)
+                    cr[i][j]='A'+(k-1);
+
+                cout<<cr[i][j];
+            }
+
+            cout<<endl;
+        }
+    }
+
+    return 0;
 }
